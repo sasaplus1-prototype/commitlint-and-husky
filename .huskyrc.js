5e0f034b4f22d3f54eda00602eaeb4ee0b010303
sasaplus1 <sasaplus1@gmail.com>
2020-10-08 01:36:40 +0900

    feat: lint with installed module

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    // 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
-    'commit-msg': 'npx -p @commitlint/cli commitlint -E HUSKY_GIT_PARAMS'
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
+    // 'commit-msg': 'npx -p @commitlint/cli commitlint -E HUSKY_GIT_PARAMS'
   }
 };
